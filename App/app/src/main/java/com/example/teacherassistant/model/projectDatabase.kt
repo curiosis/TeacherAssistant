f2b63305db5bba71cf041b6f05f30abbb92e368c
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-01-08 17:52:21 +0100

    Create database file

---
+++ App/app/src/main/java/com/example/teacherassistant/model/projectDatabase.kt
@@ -0,0 +1,57 @@
+package com.example.teacherassistant.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.teacherassistant.model.dao.CourseDao
+import com.example.teacherassistant.model.dao.MarkDao
+import com.example.teacherassistant.model.dao.StudentCourseDao
+import com.example.teacherassistant.model.dao.StudentDao
+import com.example.teacherassistant.model.entities.Course
+import com.example.teacherassistant.model.entities.Mark
+import com.example.teacherassistant.model.entities.Student
+import com.example.teacherassistant.model.entities.StudentCourse
+
+@Database(
+    entities = [
+        Student::class,
+        Mark::class,
+        Course::class,
+        StudentCourse::class
+        ],
+    version = 1,
+    exportSchema = false)
+
+
+abstract class projectDatabase: RoomDatabase() {
+
+    abstract fun studentDao(): StudentDao
+    abstract fun markDao(): MarkDao
+    abstract fun courseDao(): CourseDao
+    abstract fun studentCourseDao(): StudentCourseDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: projectDatabase? = null
+        private val NAME = "projectDatabase"
+
+        fun getDatabase(context: Context): projectDatabase{
+            val tempIstance = INSTANCE
+
+            if(tempIstance != null)
+                return tempIstance
+            else
+                synchronized(this){
+                    val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        projectDatabase::class.java,
+                        NAME
+                    ).fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                    return instance
+                }
+        }
+    }
+
+}
