1616d5e0a1f0e29587d7a470058ac2d933504eb0
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-01-09 13:59:52 +0100

    Update main activity

--- App/app/src/main/java/com/example/teacherassistant/view/fragments/MainActivity.kt
+++ App/app/src/main/java/com/example/teacherassistant/view/fragments/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.example.teacherassistant
+package com.example.teacherassistant.view.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.teacherassistant.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
