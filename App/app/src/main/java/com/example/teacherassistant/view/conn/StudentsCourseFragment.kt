373d69d1887168cc5250c1a8b04b5876f88997f3
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-01-15 01:18:35 +0100

    Update fragments

--- App/app/src/main/java/com/example/teacherassistant/view/conn/StudentsCourseFragment.kt
+++ App/app/src/main/java/com/example/teacherassistant/view/conn/StudentsCourseFragment.kt
@@ -6,17 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.teacherassistant.R
 import com.example.teacherassistant.viewModel.adapters.StudentsCourseAdapter
 import com.example.teacherassistant.viewModel.vm.CourseViewModel
 import com.example.teacherassistant.viewModel.vm.StudentCourseViewModel
 import com.example.teacherassistant.viewModel.vm.StudentViewModel
+import kotlinx.android.synthetic.main.students_course_fragment_layout.*
 
 class StudentsCourseFragment : Fragment() {
+
     lateinit var recyclerView: RecyclerView
-    lateinit var layoutManager: RecyclerView.LayoutManager
-    lateinit var adapter : StudentsCourseAdapter
+    lateinit var myLayoutManager : RecyclerView.LayoutManager
+
+    lateinit var myAdapter : StudentsCourseAdapter
+
     lateinit var studentViewModel : StudentViewModel
     lateinit var courseViewModel: CourseViewModel
     lateinit var studentCourseViewModel: StudentCourseViewModel
@@ -26,6 +32,56 @@ class StudentsCourseFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        myLayoutManager = LinearLayoutManager(context)
 
+        studentViewModel = ViewModelProvider(requireActivity())
+                .get(StudentViewModel::class.java)
+        courseViewModel = ViewModelProvider(requireActivity())
+                .get(CourseViewModel::class.java)
+        studentCourseViewModel = ViewModelProvider(requireActivity())
+                .get(StudentCourseViewModel::class.java)
+
+        studentCourseViewModel.setStudentsFCC(courseViewModel.currentCourse)
+
+        myAdapter = StudentsCourseAdapter(
+                studentCourseViewModel.studentsFromCurrentCourse!!,
+                {
+                    student -> studentCourseViewModel.deleteStudentCourse(student, courseViewModel.currentCourse!!)
+                },
+                {
+                    student -> studentViewModel.setCurrentStudent(student)
+                    studentCourseViewModel.setCurrentStudentCourse(student, courseViewModel.currentCourse)
+                })
+
+        studentCourseViewModel.studentsFromCurrentCourse!!.observe(
+                viewLifecycleOwner,{
+                    myAdapter.notifyDataSetChanged()
+        })
+
+        studentCourseViewModel.con.observe(
+                viewLifecycleOwner,{
+                    myAdapter.notifyDataSetChanged()
+        })
+
+        return inflater.inflate(R.layout.students_course_fragment_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = StsCrsRecyclerView.apply {
+            this.layoutManager = myLayoutManager
+            this.adapter = myAdapter
+        }
+
+        AddStsCrsBtn.setOnClickListener {
+            view.findNavController().navigate(
+                    R.id.action_studentsCourseFragment_to_addSCFragment
+            )
+        }
+    }
+
+    companion object{
+        fun newInstance() = StudentsCourseFragment()
     }
 }
--- App/app/src/main/java/com/example/teacherassistant/view/fragments/MarksFragment.kt
+++ App/app/src/main/java/com/example/teacherassistant/view/fragments/MarksFragment.kt
@@ -1,6 +1,26 @@
 package com.example.teacherassistant.view.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.teacherassistant.viewModel.adapters.MarkListAdapter
+import com.example.teacherassistant.viewModel.vm.CourseViewModel
+import com.example.teacherassistant.viewModel.vm.MarkViewModel
+import com.example.teacherassistant.viewModel.vm.StudentCourseViewModel
+import com.example.teacherassistant.viewModel.vm.StudentViewModel
 
 class MarksFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var myLayoutManager: RecyclerView.LayoutManager
+
+    private lateinit var myAdapter: MarkListAdapter
+
+    private lateinit var studentViewModel: StudentViewModel
+    private lateinit var courseViewModel: CourseViewModel
+    private lateinit var studentCourseViewModel: StudentCourseViewModel
+
+    private lateinit var markViewModel: MarkViewModel
+
+
+
 }
