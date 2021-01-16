6f3b420f305481e668eae24e2e86dc75fddbfd3e
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-01-16 18:00:36 +0100

    Add bottom menu

--- App/app/src/main/java/com/example/teacherassistant/view/fragments/MainActivity.kt
+++ App/app/src/main/java/com/example/teacherassistant/view/fragments/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.teacherassistant.view.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.teacherassistant.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        buttomNavigationView.setupWithNavController(navController)
     }
 }
