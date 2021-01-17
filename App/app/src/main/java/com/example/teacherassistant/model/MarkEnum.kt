0ee52ed491ad57ff32ec0ecea064899ab3ea2daf
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-01-17 15:54:39 +0100

    Update markEnum

--- App/app/src/main/java/com/example/teacherassistant/model/MarkEnum.kt
+++ App/app/src/main/java/com/example/teacherassistant/model/MarkEnum.kt
@@ -1,10 +1,10 @@
 package com.example.teacherassistant.model
 
 enum class MarkEnum (val value: Float){
-    Bdb(5f),
-    DbPlus(4.5f),
-    Db(4.0f),
-    DstPlus(3.5f),
-    Dst(3.0f),
-    Ndst(2.0f)
+    BardzoDobry(5.0f),
+    DobryPlus(4.5f),
+    Dobry(4.0f),
+    DostatecznyPlus(3.5f),
+    Dostateczny(3.0f),
+    Niedostateczny(2.0f)
 }
